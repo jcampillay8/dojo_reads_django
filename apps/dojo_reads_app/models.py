01a3c6eb962937de7c8c51da6182928681df6275
Jaime Campillay <jcampillayworks@gmail.com>
2021-05-24 13:13:53 -0400

    third commit

--- apps/dojo_reads_app/models.py
+++ apps/dojo_reads_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from apps.login_register_app.models import User
 
 # Create your models here.
+class Author(models.Model):
+    author = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, related_name='book', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Review(models.Model):
+    review = models.TextField()
+    rating = models.IntegerField(default=0)
+    book = models.ForeignKey(Book, related_name='reviews', on_delete=models.CASCADE)
+    poster = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- apps/dojo_reads_app/views.py
+++ apps/dojo_reads_app/views.py
@@ -4,11 +4,72 @@ from django.contrib import messages
 
 # Create your views here.
 def book(request):
-    
-    # if 'first_name' in request.session:
-    #     context={
-    #         'last_3_reviews': Review.objects.all().order_by('-id')[:3],
-    #         'all_books': Book.objects.all()
+    # if 'id' in request.session:
+    #     context = {
+    #         'user': User.objects.get(id=request.session['id']),
+    #         'postedMessages' : Message.objects.all(),
+    #         'postedComments' : Comment.objects.all(),
     #     }
-    #     return render(request, 'books/books_home.html', context)
+    #     print('user is in session')
+    #     return render(request, 'thewall/wall.html', context)
+    # return redirect('/')
+    if 'first_name' in request.session:
+        context={
+            'last_3_reviews': Review.objects.all().order_by('-id')[:3],
+            'all_books': Book.objects.all()
+        }
+        return render(request, 'books/books_home.html', context)
     return render(request,'books/books_home.html')
+
+def add_book(request):
+    context = {
+        'all_authors': Author.objects.all()
+    }
+    if request.method == 'POST':
+        existing_author = Author.objects.filter(author=request.POST['author'])
+        if len(existing_author) > 0:
+            this_author = existing_author[0]
+        else:
+            this_author = Author.objects.create(author=request.POST['new_author'])
+        book = Book.objects.create(title=request.POST['title'], author=this_author)
+        Review.objects.create(review=request.POST['review'], rating=request.POST['rating'], book=book, poster=User.objects.get(id=request.session['id']))
+        book_id = book.id
+        return redirect(f'/book/{book_id}')
+    return render(request, 'books/add_book.html', context)
+
+def logout(request):
+    request.session.flush()
+    return redirect('/')
+
+def one_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    context ={
+        'book': Book.objects.get(id=book_id),
+        'all_reviews': book.reviews.all()
+    }
+    return render(request, 'books/one_book.html', context)
+
+def add_review(request):
+    this_book = Book.objects.get(id=request.POST['book_id'])
+    this_user = User.objects.get(id=request.session['id'])
+    book_id = request.POST['book_id']
+    if len(request.POST['review']) < 5:
+        messages.error(request, "Review must be at least 5 characters!")
+        return redirect(f'/book/{book_id}')
+    Review.objects.create(review=request.POST['review'], rating=request.POST['rating'], book=this_book, poster=this_user)
+    return redirect(f'/book/{book_id}')
+
+def delete_review(request, review_id, book_id):
+    review_to_delete = Review.objects.get(id=review_id)
+    review_to_delete.delete()
+    return redirect(f'/book/{book_id}')
+
+def user(request, id):
+    current_user = User.objects.get(id=id)
+    # user_reviews = current_user.reviews.all()
+    # print(user_reviews[0].book.title)
+    context={
+        'user': current_user,
+        'user_reviews': current_user.reviews.all()
+    }
+    return render(request, 'books/user.html', context)
